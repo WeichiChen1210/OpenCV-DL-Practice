a69d8fdf1b278bccdf52ff499151c625c230f4a4
WeichiChen1210 <iphone31302@gmail.com>
2019-12-31 00:17:07 +0800

    Finish augmented reality(problem 4)
    1. Use given parameters(intrinsic, extrinsics, distortion and 3D points) to project 3D pyramids on 2D images.

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -9,10 +9,26 @@ mpl.use('tkAgg')
 import cv2
 import numpy as np
 import sys
+import time
+
+def draw_pyramid(img, imgpts):
+    # base
+    img = cv2.line(img, (imgpts[1][0][0],imgpts[1][0][1]),(imgpts[2][0][0],imgpts[2][0][1]), (0, 0, 255), 6)
+    img = cv2.line(img, (imgpts[2][0][0],imgpts[2][0][1]),(imgpts[3][0][0],imgpts[3][0][1]), (0, 0, 255), 6)
+    img = cv2.line(img, (imgpts[3][0][0],imgpts[3][0][1]),(imgpts[4][0][0],imgpts[4][0][1]), (0, 0, 255), 6)
+    img = cv2.line(img, (imgpts[4][0][0],imgpts[4][0][1]),(imgpts[1][0][0],imgpts[1][0][1]), (0, 0, 255), 6)
+
+    # vertex
+    for i in range(4):
+        img = cv2.line(img, (imgpts[0][0][0],imgpts[0][0][1]),(imgpts[i+1][0][0],imgpts[i+1][0][1]), (0, 0, 255), 6)
+
+    return img
 
 def draw_square(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
+        # draw red square ont the frame
         cv2.rectangle(param[0], (x-5, y-5), (x+5, y+5), (0, 0, 255), 1)
+        # save the point
         param[1].append((x, y))
 
 class Window(QWidget):
@@ -68,6 +84,7 @@ class Window(QWidget):
     def augmented_reality(self):
         groupBox = QGroupBox("4. Augmented Reality")
         push1 = QPushButton("4.1 Augmented Reality")
+        push1.clicked.connect(self.augmented)
 
         vbox = QVBoxLayout()
         vbox.addWidget(push1, 0)
@@ -79,8 +96,10 @@ class Window(QWidget):
     @pyqtSlot()
 
     def disparity(self):
-        imgL = cv2.imread('imL.png', 0)
-        imgR = cv2.imread('imR.png', 0)
+        imgL = cv2.imread('imL.png')
+        imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+        imgR = cv2.imread('imR.png')
+        imgR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 
         stereo = cv2.StereoBM_create(numDisparities=64, blockSize=9)
         disp = stereo.compute(imgL, imgR)
@@ -102,7 +121,7 @@ class Window(QWidget):
 
         # background subtractor object
         # backSub = cv2.createBackgroundSubtractorKNN()
-        backSub = cv2.createBackgroundSubtractorMOG2(history=50, varThreshold=192, detectShadows=False)
+        backSub = cv2.createBackgroundSubtractorMOG2(history=50, varThreshold=190, detectShadows=False)
         # history: number of frames to be trained, varThreshold; threshold, detectShadow: detect shadow or not
 
         while(True):
@@ -141,8 +160,10 @@ class Window(QWidget):
 
     def preprocessing(self):
         video = cv2.VideoCapture('featureTracking.mp4')
+        # take out the first frame
         ret, frame = video.read()
 
+        # parameters to be passed to mouse callback function
         param = [frame, self.points]
         cv2.namedWindow('Preprocess')
         cv2.setMouseCallback('Preprocess', draw_square, param)
@@ -150,12 +171,13 @@ class Window(QWidget):
         while True:
             cv2.imshow('Preprocess', frame)
 
+            # if 7 points, break
             if len(self.points) > 6:
                 break
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         print(self.points)
-        cv2.waitKey(1500)        
+        cv2.waitKey(1000)        
         video.release()
         cv2.destroyAllWindows()
 
@@ -219,6 +241,77 @@ class Window(QWidget):
         cv2.waitKey(1000)
         video.release()
         cv2.destroyAllWindows()
+
+    def augmented(self):
+        # 3D points
+        temp = [[3, 3, -4], [1, 1, 0], [1, 5, 0], [5, 5, 0], [5, 1, 0]]
+        objp = np.array(temp, dtype='f')
+        
+        # intrinsic
+        temp = [[2225.49585, 0, 1025.5459589],
+                [0, 2225.18414074, 1038.58518846], 
+                [0, 0, 1]]
+        mtx = np.array(temp)
+        
+        # distortion
+        temp = [[-0.12874225, 0.09057782, -0.00099125, 0.00000278, 0.0022925]]
+        distortion = np.array(temp)
+        
+        # extrinsics
+        extrinsic = []
+
+        temp = [[-0.97157425, -0.01827487, 0.23602862,  6.81253889],
+                [ 0.07148055, -0.97312723, 0.2188925,   3.37330384],
+                [ 0.22568565,  0.22954177, 0.94677165, 16.71572319]]
+        bmp1 = np.array(temp)
+        extrinsic.append(bmp1)
+
+        temp = [[-0.8884799, -0.14530922, -0.435303,    3.3925504 ],
+                [0.07148066, -0.98078915,  0.18150248,  4.36149229],
+                [-0.45331444, 0.13014556,  0.88179825, 22.15957429]]
+        bmp2 = np.array(temp)
+        extrinsic.append(bmp2)
+
+        temp = [[-0.52390938,  0.22312793, 0.82202974,  2.68774801],
+                [ 0.00530458, -0.96420621, 0.26510046,  4.70990021],
+                [ 0.85175747,  0.14324914, 0.50397308, 12.98147662]]
+        bmp3 = np.array(temp)
+        extrinsic.append(bmp3)
+
+        temp = [[-0.63108673,  0.53013053, 0.566296,    1.22781875],
+                [ 0.13263301, -0.64553994, 0.75212145,  3.48023006],
+                [ 0.76428923,  0.54976341, 0.33707888, 10.9840538 ]]
+        bmp4 = np.array(temp)
+        extrinsic.append(bmp4)
+
+        temp = [[-0.87676843, -0.23020567,  0.42223508,  4.43641198],
+                [ 0.19708207, -0.97286949, -0.12117596,  0.67177428],
+                [ 0.43867502, -0.02302829,  0.89835067, 16.24069227]]
+        bmp5 = np.array(temp)
+        extrinsic.append(bmp5)
+
+        # for each of 5 images
+        for i in range(5):
+            path = str(i+1) + '.bmp'
+            img = cv2.imread(path, -1)
+
+            # split rotation vector and translation vector
+            rvec = extrinsic[i][:, :3]
+            tvec = extrinsic[i][:, 3]
+
+            # project 3D points to image plane
+            imgpts, jac = cv2.projectPoints(objp, rvec, tvec, mtx, distortion)
+
+            # draw the pyramid
+            img = draw_pyramid(img, imgpts)
+
+            # resize
+            resize = cv2.resize(img, (700, 700))
+            cv2.imshow('Projection ' + str(i+1), resize)
+            # time.sleep(0.5)
+            cv2.waitKey(500)
+
+        return
 
 if __name__ == "__main__":
     count = 0
